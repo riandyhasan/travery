38d1572d655ca906886d4323ead9cc7a914d8104
riandyhasan <riandyhsn@gmail.com>
2023-04-10 20:57:57 +0700

    feat: splash screen

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+  black: '#000000',
+  white: '#ffffff',
+  primaryGreen: '#227466',
+  secondaryGreen: '#156B5D',
+  ternaryGreen: '#7ac8ba4d',
+  primaryGray: '#D9D9D9',
+  secondaryGray: '#F0F0F0',
+  ternaryGray: '#C0C0C0 ',
+  red: '#E74C3C',
+  gradient: {
+    primaryGreen: 'linear-gradient(88.07deg, #73C5B6 1.82%, #156B5D 125.5%)',
+    secondaryGreen: 'linear-gradient(87.47deg, #73C5B6 1.33%, #FFFFFF 96.72%)',
+    ternaryGreen: 'linear-gradient(180deg, #73C5B6 0%, #4C8F83 100%)',
+  },
+};
